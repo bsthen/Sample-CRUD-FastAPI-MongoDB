a5470291da2b9619b438ac0e85507a385163afde
“bsthen” <ban.sothen2016@rupp.edu.kh>
2022-08-13 18:18:35 +0700

    fixed _id not found

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ db = connect['db']
 table = db['db_table']
 
 #insert data into mongodb
-@app.put("/insert/{item_id}")
-async def insert(item_id: str,item_title: str):
+@app.put("/insert/{id}")
+async def insert(id: int,item_title: str):
     try:
         table.insert_one(
                 {
-                    'item_id': item_id,
+                    '_id': id,
                     'item_title': item_title,
                 }
             )
@@ -26,10 +26,10 @@ async def insert(item_id: str,item_title: str):
         return {"message": "item not inserted"}
 
 #update data into mongodb
-@app.put("/update/{item_id}")
-async def update(item_id: int, new_item_title: str):
+@app.put("/update/{id}")
+async def update(id: int, new_item_title: str):
     try:
-        data: dict = table.find_one_and_update({'item_id': item_id}, {'$set': {'item_title': new_item_title}})
+        data: dict = table.find_one_and_update({'_id': id}, {'$set': {'item_title': new_item_title}})
         if data:
             return {"message": "item updated"}
         return {"message": "item not found"}
@@ -37,10 +37,10 @@ async def update(item_id: int, new_item_title: str):
         return {"error": str(e)}
 
 #delete data from mongodb
-@app.delete("/delete/{item_id}")
-async def delete(item_id: int):
+@app.delete("/delete/{id}")
+async def delete(id: int):
     try:
-        data: dict = table.find_one_and_delete({'item_id': item_id})
+        data: dict = table.find_one_and_delete({'_id': id})
         if data:
             return {"message": "item deleted"}
         return {"message": "item not found"}
@@ -48,12 +48,12 @@ async def delete(item_id: int):
         return {"error": str(e)}
 
 #get data from mongodb
-@app.get("/get/{item_id}")
-async def get(item_id: int):
+@app.get("/get/{id}")
+async def get(id: int):
     try:
-        data: dict = table.find_one({'item_id': item_id})
+        data: dict = table.find_one({'_id': id})
         if data:
-            return {"video_id": data['item_id'], "item_title": data['item_title']}
+            return {"id": data['_id'], "item_title": data['item_title']}
         return {"message": "item not found"}
     except Exception as e:
         return {"error": str(e)}
